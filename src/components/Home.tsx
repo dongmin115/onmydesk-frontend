bb294f6abdf53ff0885106e97ab26eb554d72f62
dongmin <dongmin11566@gmail.com>
2024-03-24 18:06:19 +0900

    feat : 홈 컴포넌트 Hot Desk 섹션 퍼블리싱

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -45,14 +45,56 @@ const MenuButton = styled.button`
   border: none;
   cursor: pointer;
   font-size: 1rem;
-  transition: background-color 0.3s ease;
 
   &:hover {
-    transition: 0.5s;
+    transition: 0.5s ease;
     color: #0085ff;
   }
 `;
 
+const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 60vh;
+  padding: 2% 0 2% 0;
+`;
+
+const HotDeskTitle = styled.h1`
+  font-size: 2rem;
+  font-weight: 300;
+  color: #ffffff;
+  text-align: center;
+`;
+
+const HotDeskDescription = styled.p`
+  font-size: 1rem;
+  color: #7b7878;
+  text-align: center;
+  margin-top: 0px;
+  margin-bottom: 2%;
+`;
+
+const HotDeskImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70%;
+`;
+
+const HotDeskImage = styled.img`
+  width: 40%;
+  height: 100%;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px #000000;
+  transition: 0.5s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
 export default function Home() {
   return (
     <Container>
@@ -72,6 +114,21 @@ export default function Home() {
         <MenuButton>상품 게시판</MenuButton>
         <MenuButton>마이 페이지</MenuButton>
       </Menu>
+      <SectionContainer>
+        <HotDeskTitle>Today Hot Desk</HotDeskTitle>
+        <HotDeskDescription>
+          On My Desk에서 오늘의 인기있는 DESK를 확인해보세요!
+        </HotDeskDescription>
+        <HotDeskImageContainer>
+          <HotDeskImage
+            src="https://i.ibb.co/55RX0m5/Get-the-We-Heart-It-app-1.png"
+            alt="hot-desk"
+          />
+        </HotDeskImageContainer>
+      </SectionContainer>
+      <SectionContainer>
+        <HotDeskTitle>인기있는 셋업 상품</HotDeskTitle>
+      </SectionContainer>
     </Container>
   );
 }
