3d7ae9dfbec9d7b75c326b403f5892de582b176b
dongmin <dongmin11566@gmail.com>
2024-03-28 23:35:08 +0900

    feat : MUI ThemeProvider 적용

--- src/page/GoodsBoard.tsx
+++ src/page/GoodsBoard.tsx
@@ -1,8 +1,20 @@
 import { Button, Menu, MenuItem } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from '../components/Navbar';
 import styled from 'styled-components';
 import { KeyboardArrowDown } from '@mui/icons-material';
 import { useState } from 'react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0085FF',
+    },
+    secondary: {
+      main: '#4F4F4F',
+    },
+  },
+});
 
 // 컨테이너 스타일
 const Container = styled.div`
@@ -77,106 +89,108 @@ export default function GoodsBoard() {
     setAnchorEl(null);
   };
   return (
-    <Container>
-      <Navbar />
-      <GoodsBoardTitle>셋업에 사용된 상품을 둘러보세요!</GoodsBoardTitle>
-      <GoodsBoardMenu>
-        <Flexbox>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            전체
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            키보드
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            마우스
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            모니터
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            데스크
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            스피커
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: '15%' }}
-          >
-            기타
-          </Button>
-        </Flexbox>
-        <Flexbox2>
-          <Button
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            size="large"
-            style={{ color: '#d3d3d3', fontSize: '1rem' }}
-            endIcon={<KeyboardArrowDown />}
-          >
-            정렬기준
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-          >
-            <MenuItem onClick={handleClose}>인기순</MenuItem>
-            <MenuItem onClick={handleClose}>가격순</MenuItem>
-          </Menu>
-        </Flexbox2>
-      </GoodsBoardMenu>
-      <SetupBoardContainer>
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-        <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
-      </SetupBoardContainer>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Navbar />
+        <GoodsBoardTitle>셋업에 사용된 상품을 둘러보세요!</GoodsBoardTitle>
+        <GoodsBoardMenu>
+          <Flexbox>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              전체
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              키보드
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              마우스
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              모니터
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              데스크
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              스피커
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: '15%' }}
+            >
+              기타
+            </Button>
+          </Flexbox>
+          <Flexbox2>
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              size="large"
+              style={{ color: '#d3d3d3', fontSize: '1rem' }}
+              endIcon={<KeyboardArrowDown />}
+            >
+              정렬기준
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>인기순</MenuItem>
+              <MenuItem onClick={handleClose}>가격순</MenuItem>
+            </Menu>
+          </Flexbox2>
+        </GoodsBoardMenu>
+        <SetupBoardContainer>
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+          <SetupBoardImage src="https://i.ibb.co/4jKpMfL/2024-03-25-3-45-22.png" />
+        </SetupBoardContainer>
+      </Container>
+    </ThemeProvider>
   );
 }
