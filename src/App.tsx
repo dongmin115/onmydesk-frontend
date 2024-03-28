17508b001690b60692f9a2c2ace3b30eee373c5f
dongmin <dongmin11566@gmail.com>
2024-03-28 22:28:46 +0900

    feat : 글작성페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './page/Home';
 import Mypage from './page/Mypage';
 import SetupBoard from './page/SetupBoard';
+import PostDetail from './page/PostDetail';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<Mypage />} />
       <Route path="/setupboard" element={<SetupBoard />} />
+      <Route path="/postdetail" element={<PostDetail />} />
     </Routes>
   );
 }
