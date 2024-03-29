0b6fa6899081ec3448627bf7644fd06ac30f9e36
HSCHELO <knr0013@gmail.com>
2024-03-29 12:06:08 +0900

    feat:오류 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 // 상단바 컴포넌트
-let 상단바 = styled.header` 
-    background-color: rgba(0, 0, 0, 0.2);
-    width:100%;
-    height:7%;
-    text-align:center;
-    font-size:30px;
-    font-family: 'Courier New', Courier, monospace;
-    color:white;
-    top: 0;`
-
+let 상단바 = styled.header`
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 7%;
+  text-align: center;
+  font-size: 30px;
+  font-family: 'Courier New', Courier, monospace;
+  color: white;
+  top: 0;
+`;
 
 function Header() {
-  return (
-   <상단바>OnMyDesk</상단바>
-     );
+  return <상단바>OnMyDesk</상단바>;
 }
 
 export default Header;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,48 +1,45 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 // 모달 스타일드 컴포넌트 생성
 const ModalWrapper = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    `;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ModalContent = styled.div`
-    background-color: #A9A9A9;
-    padding: 20px;
-    border-radius: 10px;
-    width: 60vw;
-    height: 40vw;
-    margin-bottom:5vw;
+  background-color: #a9a9a9;
+  padding: 20px;
+  border-radius: 10px;
+  width: 60vw;
+  height: 40vw;
+  margin-bottom: 5vw;
 `;
 
 const ModalCloseButton = styled.button`
-    position: absolute;
-    top: 10px;
-    right: 10px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
 
 // 모달 컴포넌트 정의
 function Modal({ isOpen, onClose }) {
-    
- if(isOpen) {
-    console.log(isOpen)    //isOpen true 될때만 모달 열기
+  if (isOpen) {
+    console.log(isOpen); //isOpen true 될때만 모달 열기
     return (
-        <ModalWrapper>
-            <ModalContent>
-            
-        
-            <ModalCloseButton onClick={onClose}>닫기</ModalCloseButton>
-          </ModalContent>
-        </ModalWrapper>
+      <ModalWrapper>
+        <ModalContent>
+          <ModalCloseButton onClick={onClose}>닫기</ModalCloseButton>
+        </ModalContent>
+      </ModalWrapper>
     );
- }
+  }
 }
 
-export default Modal
+export default Modal;
