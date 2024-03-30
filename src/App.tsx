d2650f04e34357be016d70c262c5f99583edc0d9
이수민 <sammy1313@naver.com>
2024-03-30 14:31:46 +0900

    feat:상품상세페이지 퍼블리싱 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './page/Home';
 import Mypage from './page/Mypage';
 import SetupBoard from './page/SetupBoard';
+import ProductDetail from './page/ProductDetail';
+import PostDetail from './page/PostDetail';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<Mypage />} />
       <Route path="/setupboard" element={<SetupBoard />} />
+      <Route path="/postdetail" element={<PostDetail />} />
+      <Route path="/productdetail" element={<ProductDetail />} />
     </Routes>
   );
 }
