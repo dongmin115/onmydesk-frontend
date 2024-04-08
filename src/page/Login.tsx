6162092baad157971bbb38951d1fbc8aa6fc000e
이수민 <sammy1313@naver.com>
2024-04-08 21:37:09 +0900

    feat:로그인 페이지 mui 적용

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 const LoginContainer = styled.div`
   height: 100vh;
   display: flex;
@@ -15,35 +19,12 @@ const Title = styled.div`
   cursor: pointer;
 `;
 
-const Input = styled.input`
-  font-size: 1rem;
-  padding: 1.1%;
-  margin: 0.5% 0;
-  border: none;
-  border-radius: 0.3rem;
-  outline: none;
-  width: 22vw;
-  height: 2vh;
-`;
-
-const LoginButton = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 0.3rem;
-  margin-top: 1%;
-  width: 24.3vw;
-  height: 6vh;
-  cursor: pointer;
-  font-family: 'Kiwi Maru';
-  font-size: 1rem;
-`;
 const Line = styled.div`
   border: 0;
   height: 0.1vh;
   background: #fff;
   width: 24vw;
-  margin-top: 3%;
+  margin-top: 1.5%;
 `;
 const HelpTextContainer = styled.div`
   margin-top: 1%;
@@ -55,8 +36,8 @@ const HelpTextContainer1 = styled.p`
   color: #ffffff;
 `;
 
-const HelpTextContainer2 = styled.p`
-  color: #349af8;
+const HelpTextContainer2 = styled.div`
+  height: 2vh;
 `;
 const StyledLink = styled(Link)`
   // 스타일이 적용된 링크 컴포넌트 정의
@@ -77,14 +58,75 @@ const Login = () => {
       <Title>
         <StyledLink to="/">On My Desk!</StyledLink>
       </Title>
-      <Input type="text" placeholder="ID" />
-      <Input type="password" placeholder="PASSWORD" />
-      <LoginButton>Login</LoginButton>
+
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '24vw' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="ID"
+          variant="outlined"
+          sx={{
+            width: '100%',
+            '& label': {
+              color: '#ffffff',
+            },
+            '& fieldset': {
+              borderColor: 'white',
+            },
+            '&:hover fieldset': {
+              borderColor: '#349af8',
+            },
+            '& input': {
+              color: 'white',
+            },
+          }}
+        />
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '24vw' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="PASSWORD"
+          variant="outlined"
+          type="password"
+          sx={{
+            '& label': {
+              color: '#ffffff',
+            },
+            '& fieldset': {
+              borderColor: 'white',
+            },
+            '&:hover fieldset': {
+              borderColor: '#349af8',
+            },
+            '& input': {
+              color: 'white',
+            },
+          }}
+        />
+      </Box>
+      <Button variant="contained" sx={{ mt: 2, width: '24.2vw' }}>
+        Login
+      </Button>
       <Line />
       <HelpTextContainer>
         <HelpTextContainer1>계정이 없나요?</HelpTextContainer1>{' '}
         <HelpTextContainer2>
-          <StyledLink1 to="/signup">회원가입</StyledLink1>
+          <Button href="#text-buttons" sx={{ mt: 1 }}>
+            <StyledLink1 to="/signup">회원가입</StyledLink1>
+          </Button>
         </HelpTextContainer2>
       </HelpTextContainer>
     </LoginContainer>
