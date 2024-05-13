9dcb02b8fa7c9bb122e4a53a763aa439648c07e3
dongmin <dongmin11566@gmail.com>
2024-05-13 21:44:07 +0900

    fix : 셋업 업데이트 버그 수정

--- src/api/Setup.ts
+++ src/api/Setup.ts
@@ -25,28 +25,7 @@ export const postSetup = async () => {
       'http://localhost:8080/api/setups',
       {
         setupName: 'setup name',
-        products: [
-          {
-            productName:
-              'Apple 아이패드 에어 5세대 M1 WIFI 64G 스페이스 그레이 (MM9C3KH/A)',
-            img: 'https://shopping-phinf.pstatic.net/main_3153084/31530843620.20220705164247.jpg',
-            productCode: '31530843620',
-            lprice: 828490,
-            brand: 'Apple',
-            maker: 'Apple',
-            category1: '디지털/가전',
-            category2: '태블릿PC',
-            category3: 'string',
-            category4: 'string',
-            pages: [
-              {
-                price: 829690,
-                link: 'https://search.shopping.naver.com/gate.nhn?id=31530843620',
-                storeName: '네이버',
-              },
-            ],
-          },
-        ],
+        products: [],
       },
       {
         headers: {
